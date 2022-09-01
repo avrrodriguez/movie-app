2833b77fabfb7d9b9083ed6943e5cfaee94e8228
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-08-31 22:44:24 -0400

    Adds ffaker gem to generate 10 actors in seed and put in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,11 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
-actor.save
+10.times do
+  Actor.create(
+    first_name: FFaker::Name.first_name,
+    last_name: FFaker::Name.last_name,
+    known_for: FFaker::Movie.title,
+  )
+end
+puts "Done!"
