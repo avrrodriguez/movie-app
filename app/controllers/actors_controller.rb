91419f21225d879ff7a471bf7153e72414693ee1
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-09-02 20:47:35 -0400

    Adds show all actors, create, update, and delete actors routes and actions

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,6 +1,37 @@
 class ActorsController < ApplicationController
+  def index
+    actors = Actor.all
+    render json: actors.as_json
+  end
+
   def show
     actor = Actor.find_by(id: params["id"])
     render json: actor.as_json
+  end
+
+  def create
+    actor = Actor.create(
+      "first_name": params["first_name"],
+      "last_name": params["last_name"],
+      "known_for": params["known_for"],
+    )
+    render json: actor.as_json
+  end
+
+  def update
+    actor = Actor.find(params["id"])
+    actor.first_name = params["first_name"] || actor.first_name
+    actor.last_name = params["last_name"] || actor.last_name
+    actor.known_for = params["known_for"] || actor.known_for
+    actor.save
+
+    render json: actor.as_json
+  end
+
+  def delete
+    actor = Actor.find(params["id"])
+    actor.destroy
+
+    render json: { "message": "actor yeeted succcessfully" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  get "/actors", controller: "actors", action: "index"
   get "/actors/:id", controller: "actors", action: "show"
+  post "/actors", controller: "actors", action: "create"
+  patch "/actors/:id", controller: "actors", action: "update"
+  delete "/actors/:id", controller: "actors", action: "delete"
+
   get "/movies" => "movies#index"
   get "/movies/:id" => "movies#show"
   post "/movies" => "movies#create"
