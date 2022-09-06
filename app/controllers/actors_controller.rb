f84412e684e77d580d345aea0ccb104ffd98537e
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-09-06 18:58:55 -0400

    Adds gender and age to actor database

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,7 +14,10 @@ class ActorsController < ApplicationController
       "first_name": params["first_name"],
       "last_name": params["last_name"],
       "known_for": params["known_for"],
+      "gender": params["gender"],
+      "age": params["age"],
     )
+
     render json: actor.as_json
   end
 
@@ -23,6 +26,9 @@ class ActorsController < ApplicationController
     actor.first_name = params["first_name"] || actor.first_name
     actor.last_name = params["last_name"] || actor.last_name
     actor.known_for = params["known_for"] || actor.known_for
+    actor.gender = params["gender"] || actor.gender
+    actor.age = params["age"] || actor.age
+
     actor.save
 
     render json: actor.as_json
---
+++ db/migrate/20220906225509_adds_gender_and_age_to_actors.rb
@@ -0,0 +1,6 @@
+class AddsGenderAndAgeToActors < ActiveRecord::Migration[7.0]
+  def change
+    add_column :actors, :gender, :string
+    add_column :actors, :age, :integer
+  end
+end
