a2ea99ae93e9ee5968107d7b20a332a27ab636a0
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-09-09 20:37:58 -0400

    Adds authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+  # Actors routes
   get "/actors", controller: "actors", action: "index"
   get "/actors/:id", controller: "actors", action: "show"
   post "/actors", controller: "actors", action: "create"
   patch "/actors/:id", controller: "actors", action: "update"
   delete "/actors/:id", controller: "actors", action: "delete"
 
+  # Movies routes
   get "/movies" => "movies#index"
   get "/movies/:id" => "movies#show"
   post "/movies" => "movies#create"
   patch "/movies/:id" => "movies#update"
   delete "/movies/:id" => "movies#destroy"
+
+  # Users
+  post "/users", controller: "users", action: "create"
+
+  # Session
+  post "/sessions", controller: "sessions", action: "create"
 end
