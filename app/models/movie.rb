74696132dd8a168988402f8db7771749b63398da
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-09-12 19:56:43 -0400

    Adds genre model and movie_genre model to create relationship between genre and movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :plot, presence: true, length: { minimum: 30 }
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
